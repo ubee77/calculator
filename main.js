f05d0c6542d4bf37aa4cf82404d1d7549d578039
Uthsa Banerjee <uthsabanerjee52@gmail.com>
2016-03-13 15:38:31 +0530

    C, CE, back space working (primarily)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var operand1,operand2,temp="",result,operator,tempDisplay="";
+var operand1="",operand2="",temp="",result="",operator="",tempDisplay="";
 function add2str(a){
 	temp=temp+a;
 	tempDisplay=tempDisplay+a;
@@ -26,8 +26,28 @@ function calculate(){
 				break;
 		case "/":result=operand1/operand2;
 				break;
-		default:result=NaN;
+		default:result=operand2;
 	}
 	document.getElementById("disp").innerHTML=result;
 	tempDisplay="";
+}
+function C() {
+	temp="";
+	tempDisplay="";
+	document.getElementById('disp').innerHTML=tempDisplay;
+}
+function CE() {
+	tempDisplay=operand1+operator;
+	temp="";
+	document.getElementById('disp').innerHTML=tempDisplay;
+}
+function backSpace() {
+	tempDisplay=tempDisplay.substring(0,tempDisplay.length-1);
+	if(temp.length>0){
+		temp=temp.substring(0,temp.length-1);
+	}
+	else if(operand1.length>0){
+		temp=operand1;
+	}
+	document.getElementById('disp').innerHTML=tempDisplay;
 }
